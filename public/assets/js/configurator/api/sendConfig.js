7a2137d66b38c71f763d74d638e909e9d0538005
Yves Gildemeister <der.schnief@gmail.com>
2026-03-23 21:52:36 +0100

    v1.3 - Frontend - edited UI

--- public/assets/js/configurator/api/sendConfig.js
+++ public/assets/js/configurator/api/sendConfig.js
@@ -20,22 +20,38 @@ export async function sendConfig() {
     const config = configurationBuild.data;
     let html = "";
 
-    html += config.compatibility.is_valid
-        ? "✅ Configuration is VALID\n\n"
-        : "❌ Configuration is NOT valid\n\n";
+    html += ` 
+        <div class="config_result ${config.compatibility.is_valid ? "valid" : "invalid"}">
+            <div class="result_header">
+                ${config.compatibility.is_valid ? "Configuration valid" : "Configuration invalid"}
+            </div>
+
+            <div class="result_checks"></div>
+        </div>
+    `;
 
     config.compatibility.checks.forEach(check => {
         check.rules_checked.forEach(rule => {
-            html += rule.passed
-                ? `✔ ${rule.description}\n`
-                : `❌ ${rule.description}\n`;
+            html += `
+                <div class="check_item ${rule.passed ? "pass" : "fail"}" >
+                    <span class="check_icon">
+                        ${rule.passed ? "✔" : "✖"}
+                    </span>
+                    <span class="check_text">${rule.description}</span>
+                </div>
+            `;
         });
     });
 
-    html += "\n--------------------\n";
-    html += `Total Price: ${config.pricing.gross_total.toFixed(2)} ${config.pricing.currency}`;
+    html += `
+        <div class="result_footer">
+            <span>Total</span>
+            <strong>${config.pricing.gross_total.toFixed(2)} ${config.pricing.currency}</strong>
+        </div>
+    `;
 
-    clientOutput.textContent = html;
+    clientOutput.innerHTML = html;
+
 }
 
 window.sendConfig = sendConfig;
